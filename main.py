32c1a5fa87d4bdf2975e5b9d2221e9eafb305111
or <or.katz@icloud.com>
2021-09-28 13:07:14 +0300

    new changes

--- main.py
+++ main.py
@@ -2,8 +2,4 @@ from datetime import datetime
 
 print(datetime.now())
 print('i will regret on this!')
-##################################
-# this i dont want please
-# this i also dont want please
-# here i made changes
-# new changes
+print('cherry pick')

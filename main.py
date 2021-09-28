68f5bb0576917d2c2e8fc86f76006db43c096a71
or <or.katz@icloud.com>
2021-09-28 18:20:21 +0300

    ttttttttttt

--- main.py
+++ main.py
@@ -2,4 +2,4 @@ from datetime import datetime
 
 print(datetime.now())
 print('i did more new changes please build!!!!!!!!')
-print('aaaaaaa build automatically by using web hooks!!!!')
+print('aaaaaaa build gfhfghfgfgfhghf by using web hooks!!!!')
